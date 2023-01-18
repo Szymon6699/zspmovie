a628483224fa59f030aaa00b1f339ce7a895b95b
szymon6699 <szymnow2112@gmail.com>
2023-01-18 14:23:49 +0100

    2czesc1

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+
+    <head>
+
+        <link rel="stylesheet" href="css/style.css">
+        <meta charset="UTF-8">
+
+
+    </head>
+
+    <body>
+    
+            <div class="S1">
+
+                <h1>Wypożyczalnia filmów</h1>
+                <a href="sites/login.php"><input type="submit" name="przycisk" value="logowanie" size=""></a>
+                <a href="sites/register.php"><input type="submit" name="przycisk" value="rejestracja"></a>
+                <a href="sites/movie-search.php"><input type="submit" name="przycisk" value="wyszukaj film"></a>
+            </div>
+
+    </body>
+
+
+</html>
